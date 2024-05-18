c62d52085bd1ab1a82f3c4dfdf1dfd613ad3e2c6
Subham15112001 <sinchan889377888@gmail.com>
2024-05-18 15:23:14 +0530

    connected DB to port

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async ( ) => {
+    try {
+      connectionInstance =   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+      console.log(`port is connected ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log(`error : ${error}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
