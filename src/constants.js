3dc4adb534e9a339fdccff6f96a300e8ca73019d
Subham15112001 <sinchan889377888@gmail.com>
2024-05-19 08:26:28 +0530

    added dotenv and change package json to use import statement with dotenv also connected db with codebase

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "firstDB"
+export const DB_NAME = "LEARN_MONGODB"
--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 export const connectDB = async ( ) => {
     try {
-      connectionInstance =   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+     const connectionInstance =   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 
       console.log(`port is connected ${connectionInstance.connection.host}`);
     } catch (error) {
