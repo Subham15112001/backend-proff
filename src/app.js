47c27821000559d98affab830b53b451a2aac766
Subham15112001 <sinchan889377888@gmail.com>
2024-05-23 00:19:25 +0530

    created user controller and routes also added middleware of multer to upload files

--- src/app.js
+++ src/app.js
@@ -21,4 +21,13 @@ app.use(express.static("public"))
 //to read user cookies 
 app.use(cookieParser())
 
+//-----------------------------------------------------------------
+
+//routes import 
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users",userRouter)
+
 export {app}
